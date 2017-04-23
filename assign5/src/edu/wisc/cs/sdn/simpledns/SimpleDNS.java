ae4e56f5fa5e2cea391ae6fc0a6ab9904f7e7d3d
William Jen <wjen@wisc.edu>
2017-04-23 17:22:56 -0500

    Add command line arg parsing.

--- assign5/src/edu/wisc/cs/sdn/simpledns/SimpleDNS.java
+++ assign5/src/edu/wisc/cs/sdn/simpledns/SimpleDNS.java
@@ -1,9 +1,51 @@
 package edu.wisc.cs.sdn.simpledns;
 
+import java.io.File;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class SimpleDNS 
 {
 	public static void main(String[] args)
 	{
+	    InetAddress root_dns_ip = null;
+	    File ec2_csv = null;
+	    
+	    for (int i = 0; i < args.length; i++)
+	    {
+	        switch (args[i])
+	        {
+	            case "-r":
+	                if (i + 1 >= args.length)
+	                    break;
+	                
+	                try
+                    {
+	                    root_dns_ip = InetAddress.getByName(args[i + 1]);
+                    }
+                    catch (UnknownHostException e)
+                    {
+                        System.err.println("Invalid IP Address supplied!");
+                        break;
+                    }
+	                
+	            case "-e":
+	                if (i + 1 >= args.length)
+                        break;
+	                
+	                ec2_csv = new File(args[i + 1]);
+                    break;
+                    
+                default:
+                    break;
+	        }
+	    }
+	    
+	    // parse failure //
+	    if (root_dns_ip == null || ec2_csv == null || !ec2_csv.exists())
+	        return;
+	    
+	    
         System.out.println("Hello, DNS!"); 
 	}
 }
