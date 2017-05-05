fd2af6ff67adb44a5c05555c9888cdf1f7e9c134
Matt Henricks <mchenricks@wisc.edu>
2017-05-04 20:02:48 -0500

    Update Part 1 Question 2. Start on 2c

--- part1Helpers/searchJSON.sh
+++ part1Helpers/searchJSON.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 if [ $# -eq 0 ]
   then
-    echo "Usage: ./searchJSON <ip address>"
+    echo "Usage: ./searchJSON <ip address> (optional)<verbose>"
     exit
 fi
 
 ip=$1 
+verbose=$2
 
 jq '.prefixes[] | select((.service=="EC2"))' < ip-ranges.json > tmp.txt
-grep $ip tmp.txt
-
+if [ $verbose ]
+then
+  grep $ip -B 1 -A 3 tmp.txt
+else
+  grep $ip tmp.txt
+fi
 rm tmp.txt
